47d38ffdffdc4beb55176940c444d8343b3ab259
Vathsa <msshrivathsa@gmail.com>
2023-05-15 15:06:08 +0530

    Added path finding

--- quadtree.py
+++ quadtree.py
@@ -6,6 +6,9 @@ import math
 import matplotlib.pyplot as plt
 import matplotlib
 from matplotlib.collections import PatchCollection
+from dijkstra import Dijkstra
+
+stepAnimation = False
 
 class Sector:
     def __init__(self, xmin, xmax, ymin, ymax):
@@ -29,31 +32,29 @@ def checkSector(current):
     
     # check occupancy
     obstacles = len(tree.within_bb(quads.BoundingBox(min_x=current.bb[0], max_x=current.bb[1], min_y=current.bb[2], max_y=current.bb[3])))
-    if obstacles == 0: # and current.parent.reachable == True:
+    
+    # if there are no obstacles and previous sector's center was reachable
+    if obstacles == 0 and current.parent.last_reached == current.parent.center:
+        print("Skip path searching")
         current.color = 'green'
-        current.reachable = True # set current.reachable = True when its both empty and a path exists
+        current.last_reached = current.center
         animate(current.color, current)
         return
-    
-    #check reachability from last point in path to the sector's center
-    # n = len(current.path)
-    # while n:
-    #     # if checkPath(current.center, current.path[n-1]):
-    #     if True:
-    #         # Found a path
-    #         current.reachable = True
-    #         break
-    #     else:
-    #         # Check for previous point in the path
-    #         n = n - 1
 
-    # sector = current
-    # while sector:
-    #     if True: # check if path exists, but ignoring the check right now, assume every sector is reachable
-    #         current.last_reached = current.center
-    #         break
-    #     else:
-    #         sector = current.parent
+    # find a path from last_reached to current sector's center
+    sector = current
+    while sector:
+        if findPath(sector.last_reached[0], sector.last_reached[1], current.center[0], current.center[1]): # check if path exists, but ignoring the check right now, assume every sector is reachable
+            current.last_reached = current.center
+            break
+        else:
+            sector = sector.parent
+
+    # if there are no obstacles and a path exists 
+    if obstacles == 0 and current.last_reached == current.center:
+        current.color = 'green'
+        animate(current.color, current)
+        return
 
     # make an animation only checking occupancy
     # so if a sector is empty and the its p
@@ -70,12 +71,16 @@ def checkSector(current):
 
         left_bottom = Sector(xmin, xmin+child_size, ymin, ymin+child_size)
         left_bottom.parent = current
+        left_bottom.last_reached = current.last_reached
         left_top = Sector(xmin, xmin+child_size, ymin+child_size, ymax)
         left_top.parent = current
+        left_top.last_reached = current.last_reached
         right_bottom = Sector(xmin+child_size, xmax, ymin, ymin+child_size)
         right_bottom.parent = current
+        right_bottom.last_reached = current.last_reached
         right_top = Sector(xmin+child_size, xmax, ymin+child_size, ymax)
         right_top.parent = current
+        right_top.last_reached = current.last_reached
 
         sector_queue.append(left_bottom)
         sector_queue.append(left_top)
@@ -86,8 +91,57 @@ def checkSector(current):
 
 rectangles = []
 
+def generateGridMap(points, resolution, margin):
+    # Find the minimum and maximum x and y coordinates in the list of points
+    # min_x = min(points, key=lambda p: p[0])[0]
+    # max_x = max(points, key=lambda p: p[0])[0]
+    # min_y = min(points, key=lambda p: p[1])[1]
+    # max_y = max(points, key=lambda p: p[1])[1]
+
+    # Set min and max to map_size
+    min_x = -map_size/2
+    min_y = -map_size/2
+    max_x = map_size/2
+    max_y = map_size/2
+
+    # Add the margin to the boundaries of the grid
+    min_x -= margin
+    max_x += margin
+    min_y -= margin
+    max_y += margin
+
+    # Calculate the width and height of the grid map
+    width = int((max_x - min_x) / resolution) + 1
+    height = int((max_y - min_y) / resolution) + 1
+
+    # Initialize the grid map with all zeros
+    grid_map = [[0 for j in range(width)] for i in range(height)]
+
+    # Convert each point to a cell index in the grid map and set it to 1
+    for point in points:
+        x = int((point[0] - min_x) / resolution)
+        y = int((point[1] - min_y) / resolution)
+        grid_map[y][x] = 1
+
+    return grid_map
+
+def findPath(sx, sy, gx, gy):
+    global pathCount
+    pathCount = pathCount + 1
+    print(pathCount)
+    # print(sx, sy, gx, gy)
+    rx, ry = planner.planning(sx, sy, gx, gy)
+    if rx == -1:
+        # print("Not found path")
+        return False
+    # print("Found a path")
+    return True
+
 def animate(c, current):
-    print(c)
+
+    rectangles.append(plt.Rectangle((current.center[0]-current.size/2, current.center[1]-current.size/2), current.size, current.size, fill=True, facecolor=c, edgecolor='black'))
+    if not stepAnimation:
+        return
     fig, ax = plt.subplots()
     ax.axis('equal')
     ax.add_patch(plt.Rectangle((-20, -20), 40, 40, fill=True, facecolor='white'))
@@ -95,15 +149,10 @@ def animate(c, current):
     # if current.parent:
     #     rectangles.append(plt.Rectangle((current.parent.center[0]-current.parent.size/2, current.parent.center[1]-current.parent.size/2), current.parent.size, current.parent.size
     #                             , fill=True, facecolor=current.parent.color, edgecolor='black'))
-    rectangles.append(plt.Rectangle((current.center[0]-current.size/2, current.center[1]-current.size/2), current.size, current.size, fill=True, facecolor=c, edgecolor='black'))
-
     # if current.parent:
     #     ax.add_patch(plt.Rectangle((current.parent.center[0]-current.parent.size/2, current.parent.center[1]-current.parent.size/2), current.parent.size, current.parent.size
     #                             , fill=True, facecolor=current.parent.color, edgecolor='black'))
     # ax.add_patch(plt.Rectangle((current.center[0]-current.size/2, current.center[1]-current.size/2), current.size, current.size, fill=True, facecolor=c, edgecolor='black'))
-
-    for r in rectangles:
-        print(r)
 
     patches_collection = PatchCollection(rectangles, match_original=True)
     ax.add_collection(patches_collection)
@@ -112,6 +161,23 @@ def animate(c, current):
     plt.ylim(-30, 30)
     plt.scatter(current.center[0], current.center[1])
     plt.scatter(all_points[:, 0], all_points[:, 1])
+
+    # Add last reached
+    if current.parent:
+        plt.plot(current.parent.last_reached[0], current.parent.last_reached[1], '.k')
+    plt.show()
+
+def showFinal():
+    fig, ax = plt.subplots()
+    ax.axis('equal')
+    ax.add_patch(plt.Rectangle((-20, -20), 40, 40, fill=True, facecolor='white'))
+    patches_collection = PatchCollection(rectangles, match_original=True)
+    ax.add_collection(patches_collection)
+
+    plt.xlim(-30, 30)
+    plt.ylim(-30, 30)
+    plt.scatter(all_points[:, 0], all_points[:, 1])
+
     plt.show()
 
 def checkIntersection(x, y):
@@ -135,6 +201,7 @@ def checkIntersection(x, y):
     return True
 
 if __name__ == "__main__":
+    # Import points data
     with open('input_data.json', 'r') as f:
         data = json.load(f)
 
@@ -151,11 +218,13 @@ if __name__ == "__main__":
         x_points.append(data['uav1'][0][i][0][0])
         y_points.append(data['uav1'][0][i][0][1])
 
+    # PARAMS
     drone_radius = 1
     map_size = 40 #Side Length of Map of Square shape
     depth = math.floor(math.log2(map_size/drone_radius))
 
-    # spindex = Index(bbox=(-20, 20, -20, 20), max_items=len(points), max_depth=depth)
+
+    # Create a quadtree for querying points
     tree = quads.QuadTree((0, 0), map_size, map_size)
 
     for p in points:
@@ -165,14 +234,22 @@ if __name__ == "__main__":
 
     all_points = np.array(points)
 
+    # Initialize sectors
     current_sector = Sector(-map_size/2, map_size/2, -map_size/2, map_size/2)
-
     init_point = [-15, -6]
     current_sector.path.append(init_point)
+    current_sector.last_reached = init_point
     sector_queue = [current_sector]
+
+    # Djikstra 
+    planner = Dijkstra(all_points[:,0], all_points[:,1], 1.0, 1.0, 40, animate=False)
+    pathCount = 0
     
+    # Start from the first sector
     while len(sector_queue) > 0:
         checkSector(sector_queue.pop(0))
+
+    showFinal()
 
 
     
